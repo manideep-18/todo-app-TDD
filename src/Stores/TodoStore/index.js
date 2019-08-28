f853184610c42d7c37a2a36af5f48236889afa82
18121997 <r8manideep@gmail.com>
2019-08-28 16:07:31 +0530

    [Refactor State] addTodo refactored

--- src/Stores/TodoStore/index.js
+++ src/Stores/TodoStore/index.js
@@ -1,10 +1,12 @@
-import { action } from "mobx";
+import { action, observable } from "mobx";
 import Todo from "../Model/Todo";
 
 class TodoStore{
+    @observable todos=[];
     @action addTodo=(description)=>{
         const todo=new Todo();
         todo.setTodoDescription(description);
+        this.todos.push(todo);
     }
 }
 export default TodoStore;
