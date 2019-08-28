a6be5f0c2d2b6c5dcace74fff356a659ed1d1468
18121997 <r8manideep@gmail.com>
2019-08-28 15:14:00 +0530

    [Refactor state] setTodoDescription refactoring completed

--- src/Stores/Model/Todo/index.js
+++ src/Stores/Model/Todo/index.js
@@ -2,6 +2,9 @@ import { action, observable } from "mobx";
 
 class Todo {
   @observable todoDescription;
+  constructor() {
+    this.todoDescription = "";
+  }
   @action setTodoDescription = description => {
     this.todoDescription = description;
   };
