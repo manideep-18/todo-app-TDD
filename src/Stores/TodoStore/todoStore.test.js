ad3ca0b75d4ad5fd20487fa1c99e7069733015bb
18121997 <r8manideep@gmail.com>
2019-08-28 16:21:06 +0530

    TodoStore scrap deleted

--- src/Stores/TodoStore/todoStore.test.js
+++
@@ -1,8 +0,0 @@
-import TodoStore from ".";
-
-describe('todoStore testsuit', () => {
-    it('should test addTodo function',()=>{
-        const todoStore=new TodoStore(); 
-        todoStore.addTodo('mani');
-    })
-})
