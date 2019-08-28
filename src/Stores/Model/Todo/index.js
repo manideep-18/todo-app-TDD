56db8709b3b25e3b10d5a4259f91d70632a06b7e
18121997 <r8manideep@gmail.com>
2019-08-28 14:56:52 +0530

    [Red State] write a testcase for setTodoDescription function

---
+++ src/Stores/Model/Todo/index.js
@@ -0,0 +1,6 @@
+import { action } from "mobx";
+
+class Todo {
+  @action setTodoDescription = () => {};
+}
+export default Todo;
---
+++ src/Stores/Model/Todo/todoModel.test.js
@@ -0,0 +1,9 @@
+import Todo from ".";
+
+describe("TodoModel testsuit", () => {
+  it("should test setTodoDescription ", () => {
+    const todo = new Todo();
+    todo.setTodoDescription("learn-tdd");
+    expect(todo.todoDescripton).toBe("learn-tdd");
+  });
+});
