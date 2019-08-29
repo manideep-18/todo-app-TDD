7dbfec5292ef7b17b032eee6e0adb53029d351c2
18121997 <r8manideep@gmail.com>
2019-08-29 15:51:49 +0530

    [Refactored State] TodoInput component refactored

--- src/TodoInput/index.js
+++ src/TodoInput/index.js
@@ -2,17 +2,19 @@ import React, { Component } from "react";
 import { TodoInputBg, TodoInputBox } from "./styledComponents";
 
 class TodoInput extends Component {
-  value;
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+  }
   handleChange = event => {
-    this.value = event.target.value;
+    this.setState({ value: event.target.value });
   };
   handleKeyDown = event => {
-    this.value = this.value.trim();
+    const str = this.state.value.trim();
     if (event.key === "Enter") {
-      if (this.value !== "") {
-        this.props.todoInputChange(this.value);
-        this.value = "";
-        // this.props.todoInputChange(this.value);
+      if (str !== "") {
+        this.props.todoInputChange(this.state.value);
+        this.setState({ value: "" });
       }
     }
   };
@@ -20,7 +22,7 @@ class TodoInput extends Component {
     return (
       <TodoInputBg>
         <TodoInputBox
-          value={this.value}
+          value={this.state.value}
           type="text"
           placeholder="what needs to be done..."
           onChange={this.handleChange}
