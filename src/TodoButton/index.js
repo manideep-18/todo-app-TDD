29a3c31781782c1947e86e6c0fb769cbfe887277
18121997 <r8manideep@gmail.com>
2019-08-29 10:36:33 +0530

    [Refactor State]  StyledTodoButton UI rendered refactored

--- src/TodoButton/index.js
+++ src/TodoButton/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { TodoButtonBg, StyledTodoButton } from "./styledComponents";
-
+import { inject } from "mobx-react";
+@inject("todoStore")
 class TodoButton extends Component {
   render() {
     return (
--- src/TodoButton/todoButton.test.js
+++ src/TodoButton/todoButton.test.js
@@ -1,9 +1,16 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import TodoButton from ".";
+import TodoStore from "../Stores/Model/TodoStore";
+import { Provider } from "mobx-react";
 describe("TodoButton testsuit", () => {
   it("should test StyledTodoButton rendered in UI", () => {
-    const { getByText } = render(<TodoButton todoButtonText="All" />);
+    const todoStore = new TodoStore();
+    const { getByText } = render(
+      <Provider todoStore={todoStore}>
+        <TodoButton todoButtonText="All" />
+      </Provider>
+    );
     const todoButton = getByText("All");
     expect(todoButton).toBeDefined();
   });
