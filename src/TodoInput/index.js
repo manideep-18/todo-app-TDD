ca1d1fb06ae01acce001005c5a84975dad119756
18121997 <r8manideep@gmail.com>
2019-08-29 10:09:43 +0530

    [Red State] write a test for TodoInput handleKeyDown function

--- src/TodoInput/index.js
+++ src/TodoInput/index.js
@@ -1,13 +1,29 @@
-import React, { Component } from 'react'
-import { TodoInputBg, TodoInputBox } from './styledComponents';
+import React, { Component } from "react";
+import { TodoInputBg, TodoInputBox } from "./styledComponents";
 
- class TodoInput extends Component {
-    render() {
-        return (
-            <TodoInputBg>
-                <TodoInputBox   type="text" placeholder="what needs to be done..." />
-            </TodoInputBg>
-        )
+class TodoInput extends Component {
+  value = "";
+  handleChange = event => {
+    this.value = event.target.value;
+  };
+  handleKeyDown = event => {
+    if (event.key === "Enter") {
+      this.value = "";
+      //   this.props.todoInputChange();
     }
+  };
+  render() {
+    return (
+      <TodoInputBg>
+        <TodoInputBox
+          value={this.value}
+          type="text"
+          placeholder="what needs to be done..."
+          onChange={this.handleChange}
+          onKeyDown={this.handleKeyDown}
+        />
+      </TodoInputBg>
+    );
+  }
 }
 export default TodoInput;
--- src/TodoInput/todoInputTestcases.test.js
+++ src/TodoInput/todoInputTestcases.test.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import {render, fireEvent} from '@testing-library/react';
-import TodoInput from '.';
-describe('TodoInput Testsuit', () => {
-    it('should test TodoInputBox is rendered',()=>{
-        const {getByPlaceholderText}=render(<TodoInput/>)
-        const todoInput=getByPlaceholderText('what needs to be done...');
-        expect(todoInput).toBeDefined();
-    })
-})
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import TodoInput from ".";
+describe("TodoInput Testsuit", () => {
+  it("should test TodoInputBox is rendered", () => {
+    const { getByPlaceholderText } = render(<TodoInput />);
+    const todoInput = getByPlaceholderText("what needs to be done...");
+    expect(todoInput).toBeDefined();
+  });
+  it("should test TodoInputBox handleChange", () => {
+    const todoInputChange = jest.fn();
+    const { getByPlaceholderText } = render(
+      <TodoInput todoInputChange={todoInputChange} />
+    );
+    const todoInput = getByPlaceholderText("what needs to be done...");
+    fireEvent.change(todoInput);
+    fireEvent.keyDown(todoInput, { key: "Enter", code: 13 });
+    expect(todoInputChange).toBeCalled();
+  });
+});
